5a8799826fbd7b2530baa2cc4a9deb77c2f7442a
Erka Ekanon <erkaekanon@outlook.com>
2024-11-06 02:22:18 +0100

    .

--- killstory/__init__.py
+++ killstory/__init__.py
@@ -1,20 +1,10 @@
 # killstory/__init__.py
-
 """Killstory plugin app for Alliance Auth."""
 
 import logging
-from django.apps import apps
 from .app_settings import KILLSTORY_LOG_LEVEL
 
-# Set up logging for this module based on the specified log level
+# Configure logging pour ce module
 logging.basicConfig(level=getattr(logging, KILLSTORY_LOG_LEVEL, "INFO"))
-
-# Set the default application configuration
-default_app_config = "killstory.apps.KillstoryConfig"
-
-# Import tasks only when apps are ready
-def ready():
-    if apps.ready:
-        from . import tasks  # noqa: F401
 
 __version__ = "0.0.1"
--- killstory/apps.py
+++ killstory/apps.py
@@ -3,8 +3,9 @@
 from django.apps import AppConfig
 
 class KillstoryConfig(AppConfig):
-    name = 'killstory'
-    verbose_name = 'Killstory'
+    name = "killstory"
+    verbose_name = "Killstory"
 
     def ready(self):
-        import killstory.tasks  # Importer les tâches lors de la préparation de l'application
+        # Import des tâches pour que Celery les découvre correctement
+        import killstory.tasks  # noqa: F401
